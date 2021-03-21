a90445758f9a603d96b58403f96d78edd1131134
lewatt23 <lewatt23@gmail.com>
2021-03-21 23:05:04 +0100

    added: actions and actiontypes for CRUD of notes and notesbooks

--- src/screens/NoteBook/actions.ts
+++ src/screens/NoteBook/actions.ts
@@ -1,6 +1,44 @@
-import {AppActionTypes, APP_SAVED_LATEST} from './types';
+import {
+  ADD_NOTE,
+  ADD_NOTEBOOK,
+  ALL_GET_NOTE_ON_NOTEBOOK,
+  AppActionTypes,
+  DELETE_NOTE,
+  DELETE_NOTEBOOK,
+  EDIT_NOTE,
+  EDIT_NOTEBOOK,
+  INote,
+  INoteBook,
+} from './types';
 
-export const appSavedLatestAction = (payload: string): AppActionTypes => ({
-  type: APP_SAVED_LATEST,
+export const noteBookAddAction = (payload: INoteBook): AppActionTypes => ({
+  type: ADD_NOTEBOOK,
+  payload: payload,
+});
+export const noteBookEditAction = (payload: INoteBook): AppActionTypes => ({
+  type: EDIT_NOTEBOOK,
+  payload: payload,
+});
+
+export const noteBookDeleteAction = (payload: number): AppActionTypes => ({
+  type: DELETE_NOTEBOOK,
+  payload: payload,
+});
+
+export const noteAddAction = (payload: INote): AppActionTypes => ({
+  type: ADD_NOTE,
+  payload: payload,
+});
+export const noteEditAction = (payload: INote): AppActionTypes => ({
+  type: EDIT_NOTE,
+  payload: payload,
+});
+
+export const noteDeleteAction = (payload: number): AppActionTypes => ({
+  type: DELETE_NOTE,
+  payload: payload,
+});
+export const noteGetAllAction = (payload: number): AppActionTypes => ({
+  type: ALL_GET_NOTE_ON_NOTEBOOK,
   payload: payload,
 });
--- src/screens/NoteBook/types.ts
+++ src/screens/NoteBook/types.ts
@@ -1,12 +1,94 @@
+import {TagEnum} from './component/card';
 export const APP_SAVED_LATEST = ' APP_SAVED_LATEST';
+//Notebook
+export const ADD_NOTEBOOK = 'ADD_NOTEBOOK';
+export const EDIT_NOTEBOOK = 'EDIT_NOTEBOOK';
+export const DELETE_NOTEBOOK = 'DELETE_NOTEBOOK';
+export const GET_NOTEBOOK = 'GET_NOTEBOOK';
+export const ALL_GET_NOTEBOOK = 'ALL_GET_NOTEBOOK';
+//NotePage
+export const ADD_NOTE = 'ADD_NOTE';
+export const EDIT_NOTE = 'EDIT_NOTE';
+export const DELETE_NOTE = 'DELETE_NOTE';
+export const GET_NOTE = 'GET_NOTE';
+export const ALL_GET_NOTE_ON_NOTEBOOK = 'ALL_GET_NOTE_ON_NOTEBOOK';
 
-export interface AppSavedLatestAction {
-  type: typeof APP_SAVED_LATEST;
-  payload: string;
+export interface INoteBook {
+  id: number;
+  title: string;
+  description: string;
+  tag: TagEnum | string;
 }
 
-export type AppActionTypes = AppSavedLatestAction | {type: 'hdhs'};
+export interface INote {
+  id: number;
+  pid: number;
+  title: string;
+  description: string;
+  tag: TagEnum | string;
+  createdAt: string;
+}
+export interface NoteBookAddAction {
+  type: typeof ADD_NOTEBOOK;
+  payload: INoteBook;
+}
+
+export interface NoteBookGetAction {
+  type: typeof GET_NOTEBOOK;
+  payload: number;
+}
+
+export interface NoteBookdDeleteAction {
+  type: typeof DELETE_NOTEBOOK;
+  payload: number;
+}
+
+export interface NoteBookdEditeAction {
+  type: typeof EDIT_NOTEBOOK;
+  payload: INoteBook;
+}
+export interface NoteBookGetAllAction {
+  type: typeof ALL_GET_NOTEBOOK;
+}
+
+export interface NoteGetAllAction {
+  type: typeof ALL_GET_NOTE_ON_NOTEBOOK;
+  payload: number;
+}
+
+export interface NoteAddAction {
+  type: typeof ADD_NOTE;
+  payload: INote;
+}
+
+export interface NoteGetAction {
+  type: typeof GET_NOTE;
+}
+
+export interface NoteDeleteAction {
+  type: typeof DELETE_NOTE;
+  payload: number;
+}
+
+export interface NoteEditeAction {
+  type: typeof EDIT_NOTE;
+  payload: INote;
+}
 
 export interface AppState {
-  saved: any | undefined;
+  NoteBooks: INoteBook[];
+  Note: INote[];
+  PNote: INote[];
 }
+
+export type AppActionTypes =
+  | NoteBookAddAction
+  | NoteBookGetAction
+  | NoteBookGetAllAction
+  | NoteBookdDeleteAction
+  | NoteBookdEditeAction
+  | NoteAddAction
+  | NoteEditeAction
+  | NoteGetAllAction
+  | NoteDeleteAction
+  | NoteGetAction;
